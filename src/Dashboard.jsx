9eeb2029f4d0c1e9783fc26b2f198187642fdbc4
Pranab Amatya <PranabAmatya@arn.com.au>
2022-05-16 16:15:30 +1000

    Regions

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -13,6 +13,8 @@ const Dashboard = () => {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState('');
   const [steps, setStep] = useState('');
+  const [region, setRegion] = useState('');
+  const [group , setGroup] = useState('');
   
 
   const navigate = useNavigate();
@@ -24,7 +26,8 @@ const Dashboard = () => {
       const data = doc.docs[0].data();
       setName(data.name);
       setStep(data.totalSteps)
-      console.log(data)
+      setRegion(data.region)
+      setGroup(data.group)
     } catch (err) {
       console.error(err);
       alert('An error occured while fetching user data');
@@ -41,19 +44,33 @@ const Dashboard = () => {
    navigate('/update')
   };
 
+  const regionData = (region) => {
+    navigate(`/region/${region}`)
+  }
+
   return (
     <div className='dashboard'>
        <div className='dashboard_container'>
         Logged in as
          <div>{name}</div>
          <div>{user?.email}</div>
-         <div>{steps}</div>
-         <button className='dashboard_btn update_btn' onClick={updateSteps}>
-          Update
-         </button>
+         <div> Your Region: <p onClick={() => regionData(region)}>{region}</p></div>
+         <div>Your Group: <p href='/'>{group}</p></div>
          <button className='dashboard_btn' onClick={logout}>
           Logout
          </button>
+       </div>
+       
+       <div className='current_status_card'>
+          <div className='title'>
+            <h2>Your Steps</h2>
+          </div>
+          <div className='stats'>
+              <h4>Current Steps: {steps}</h4>
+              <button className='dashboard_btn update_btn' onClick={updateSteps}>
+          Update
+         </button>
+          </div>
        </div>
     </div>
   );
