3f285db5709751157e12ff4a401c2193e15948ef
Pranab Amatya <PranabAmatya@arn.com.au>
2022-05-18 11:03:44 +1000

    Styling

--- src/Component/Group.jsx
+++ src/Component/Group.jsx
@@ -4,17 +4,20 @@ import { useParams } from "react-router-dom";
 import { auth, db, logout } from '../Firebase';
 import { query, collection, getDocs, where } from 'firebase/firestore';
 
-import './Region.scss'
+import './Group.scss'
 
 const Group = () => {
 
     const group = useParams();
 
     const [groupData, setGroupData] = useState([])
+    const [groupTotal, setGroupTotoal] = useState(null);
+    const [overallTotal, setOverallTotal] = useState(null);
 
     const fetchGroupData = async () => {
 
         let res = [];
+        let total = 0;
 
         try {
           const q = query(collection(db, 'users'), where( 'group', '==' , group.group));
@@ -24,6 +27,7 @@ const Group = () => {
           data.forEach( user => {
             let result = user.data();
             res.push({name: result.name, steps: result.totalSteps})
+            total += result.totalSteps
           })
         } catch (err) {
           console.error(err);
@@ -31,25 +35,76 @@ const Group = () => {
         }  
 
         setGroupData(res)
+        setGroupTotoal(total)
+    };
+
+    const fetchOverallTotal = async () => {
+        let total = 0;
+    
+        try {
+          const q = query(collection(db, 'users'));
+          const doc = await getDocs(q);
+          const data = doc.docs;
+    
+          data.forEach((user) => {
+            let result = user.data();
+            total += result.totalSteps;
+          });
+        } catch (err) {
+          console.error(err);
+          alert('An error occured while fetching user data');
+        }
+        setOverallTotal(total);
     };
 
     useEffect(() => {
         fetchGroupData();
-      }, []);
+        fetchOverallTotal();
+    }, []);
 
     return(
         <div className="group_container">
-            <h3>{group.group} Leaderboard</h3>
-            <ul className="list_container">
-                {groupData.map(item => {
-                    return (
-                        <li  className="list_item" key={groupData.indexOf(item)}>
-                            <h4>{item.name}</h4>
-                            <p>{item.steps}</p>
-                        </li>
-                    )
-                })}
+             <div className='content'>
+        <div className='card_container'>
+        <div className='card'>
+        <div className='card_blue'>
+            <div className='content'>
+            <h2 className='title'>{group.group} total Steps</h2>
+            <h1>{groupTotal}</h1>
+            </div>
+          </div>
+        </div>
+         <div className='card'>
+         <div className='card_grey'>
+             <div className='content'>
+             <h2 className='title'>Overall Total Steps</h2>
+            <h1>{overallTotal}</h1>
+             </div>
+            
+          </div>
+         </div>
+         
+        </div>
+
+    
+
+        <div className='group_leaderboard'>
+          <div className='content'>
+            <h4>{group.group} Group Leaderboard</h4>
+            <ul className='list_container'>
+              {groupData.map((item) => {
+                return (
+                  <li className='list_item' key={groupData.indexOf(item)}>
+                    <h4>{groupData.indexOf(item) + 1}. {item.name}</h4>
+                    <span class="separator"></span>
+                    <p>{item.steps}</p>
+                  </li>
+                );
+              })}
             </ul>
+          </div>
+        </div>
+      </div>
         </div>
     )
 }
--- src/Component/Region.jsx
+++ src/Component/Region.jsx
@@ -1,57 +1,132 @@
-import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import { auth, db, logout } from '../Firebase';
 import { query, collection, getDocs, where } from 'firebase/firestore';
 
-import './Region.scss'
+import './Region.scss';
 
 const Region = () => {
+  const region = useParams();
 
-    const region = useParams();
+  const [regionData, setRegionData] = useState([]);
+  const [regionalTotal, setRegionalTotal] = useState(null);
+  const [overallTotal, setOverallTotal] = useState(null);
+  const [regionalGroupData, setRegionalGroupData] = useState([]);
 
-    const [regionData, setRegionData] = useState([])
+  const fetchRegionalUserData = async () => {
+    let res = [];
+    let groupData = [];
+    let total = 0;
 
-    const fetchRegionalData = async () => {
+    try {
+      const q = query(
+        collection(db, 'users'),
+        where('region', '==', region.region)
+      );
+      const doc = await getDocs(q);
+      const data = doc.docs;
 
-        let res = [];
+      data.forEach((user) => {
+        let result = user.data();
+        res.push({ name: result.name, steps: result.totalSteps });
+        groupData.push({ name: result.group, steps: result.totalSteps });
+        total += result.totalSteps;
+      });
+    } catch (err) {
+      console.error(err);
+      alert('An error occured while fetching user data');
+    }
 
-        try {
-          const q = query(collection(db, 'users'), where( 'region', '==' , region.region));
-          const doc = await getDocs(q);
-          const data = doc.docs;
+    setRegionalTotal(total);
+    setRegionData(res);
+    setRegionalGroupData(groupData);
+  };
 
-          data.forEach( user => {
-            let result = user.data();
-            res.push({name: result.name, steps: result.totalSteps})
-          })
-        } catch (err) {
-          console.error(err);
-          alert('An error occured while fetching user data');
-        }  
+  const fetchOverallTotal = async () => {
+    let total = 0;
 
-        setRegionData(res)
-    };
+    try {
+      const q = query(collection(db, 'users'));
+      const doc = await getDocs(q);
+      const data = doc.docs;
 
-    useEffect(() => {
-        fetchRegionalData();
-      }, []);
+      data.forEach((user) => {
+        let result = user.data();
+        total += result.totalSteps;
+      });
+    } catch (err) {
+      console.error(err);
+      alert('An error occured while fetching user data');
+    }
+    setOverallTotal(total);
+  };
 
-    return(
-        <div className="region_container">
-            <h3>{region.region} Leaderboard</h3>
-            <ul className="list_container">
-                {regionData.map(item => {
-                    return (
-                        <li  className="list_item" key={regionData.indexOf(item)}>
-                            <h4>{item.name}</h4>
-                            <p>{item.steps}</p>
-                        </li>
-                    )
-                })}
-            </ul>
+  useEffect(() => {
+    fetchRegionalUserData();
+    fetchOverallTotal();
+  }, []);
+
+  return (
+    <div className='region_container'>
+      <div className='content'>
+        <div className='card_container'>
+        <div className='card'>
+        <div className='card_blue'>
+            <div className='content'>
+            <h2 className='title'>{region.region} total Steps</h2>
+            <h1>{regionalTotal}</h1>
+            </div>
+          </div>
         </div>
-    )
-}
+         <div className='card'>
+         <div className='card_grey'>
+             <div className='content'>
+             <h2 className='title'>Overall Total Steps</h2>
+            <h1>{overallTotal}</h1>
+             </div>
+            
+          </div>
+         </div>
+         
+        </div>
+
+        <div className='region_leaderboard'>
+          <div className='content'>
+            <h4>{region.region} Leaderboard</h4>
+            <ul className='list_container'>
+              {regionData.map((item) => {
+                return (
+                  <li className='list_item' key={regionData.indexOf(item)}>
+                    <h4>{regionData.indexOf(item) + 1}. {item.name}</h4>
+                    <span class="separator"></span>
+                    <p>{item.steps}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+
+        <div className='group_leaderboard'>
+          <div className='content'>
+            <h4>{region.region} Group Leaderboard</h4>
+            <ul className='list_container'>
+              {regionalGroupData.map((item) => {
+                return (
+                  <li className='list_item' key={regionalGroupData.indexOf(item)}>
+                    <h4>{regionalGroupData.indexOf(item) + 1}. {item.name}</h4>
+                    <span class="separator"></span>
+                    <p>{item.steps}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Region;
